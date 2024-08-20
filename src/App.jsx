11dfbfb646f4360e61c5c1968bb9eebd1d92ebf3
TP-WN821N <azikmelor7705@gmail.com>
2024-08-20 22:07:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='text-3xl text-red-600'>Hello World</h1>
+      </div>
+    )
+  }
+}
